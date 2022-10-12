669bc7fe31a4ef2ca20a9281251c405b98f58d9f
Parseltang <eshenkove@gmail.com>
2022-10-12 16:29:18 +0500

    Add F.py

--- F.py
+++ F.py
@@ -6,15 +6,15 @@ if AFM == "a":
 	F=input("F=")
 	m=input("m=")
 	a= float(F)/float(m)
-	input('a='+a)
+	input('a='+str(a))
 elif AFM == "F":
 	m=input("m=")
 	a=input("a=")
 	F= float(m)*float(a)
-	input('F='+F)
+	input('F='+str(F))
 if AFM == "m":
 	F=input("F=")
 	a=input("a=")
 	m=float(F)/float(a)
-	input('m='+m)
+	input('m='+str(m))
 os.system('clear||cls')
