72153466d67a2bfaaed5f10e83fb9bf658119ee1
Parseltang <eshenkove@gmail.com>
2022-10-12 19:15:22 +0500

    Add F.py

--- F.py
+++ F.py
@@ -1,20 +1,48 @@
 import math
 import os
 os.system('clear||cls')
-AFM = input("Что найти?(a,F,m):")
-if AFM == "a":
-	F=input("F=")
-	m=input("m=")
-	a= float(F)/float(m)
-	input('a='+str(a))
-elif AFM == "F":
-	m=input("m=")
-	a=input("a=")
-	F= float(m)*float(a)
-	input('F='+str(F))
-if AFM == "m":
-	F=input("F=")
-	a=input("a=")
-	m=float(F)/float(a)
-	input('m='+str(m))
+print("a;F;m1")
+print("V;Ro;m2)")
+Formula = input("Что найти?:")
+#F a m
+if Formula == "a":
+	a=input("F=")
+	b=input("m=")
+	c= float(a)/float(b)
+	input('a='+str(c))
+elif Formula == "F":
+	a=input("m=")
+	b=input("a=")
+	c= float(a)*float(b)
+	input('F='+str(c))
+elif Formula == "m1":
+	a=input("F=")
+	b=input("a=")
+	c=float(a)/float(b)
+	input('m='+str(c))
+#V Ro m
+elif Formula== "V":
+	a=input("m=")
+	b=input("Ro=")
+	c=float(a)/float(b)
+	input('V='+str(c))
+elif Formula == "Ro":
+	a=input("m=")
+	b=input("V=")
+	c=float(a)*float(b)
+	input('m='+str(c))
+elif Formula == "m1":
+	a=input("Ro=")
+	b=input("V=")
+	c=float(a)*float(b)
+	input('m='+str(c))
+
+
+
+
+
+
+
+
+
 os.system('clear||cls')
